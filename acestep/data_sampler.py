6c00e994a18fc293430117a2d4fd427e5b6bcd07
jarammm <jaramrammm@gmail.com>
2025-07-18 11:43:15 +0900

    u: change datasampler to test custom lora

--- acestep/data_sampler.py
+++ acestep/data_sampler.py
@@ -4,27 +4,27 @@ import random
 
 
 DEFAULT_ROOT_DIR = "examples/default/input_params"
-ZH_RAP_LORA_ROOT_DIR = "examples/zh_rap_lora/input_params"
 
 class DataSampler:
-    def __init__(self, root_dir=DEFAULT_ROOT_DIR):
-        self.root_dir = root_dir
-        self.input_params_files = list(Path(self.root_dir).glob("*.json"))
-        self.zh_rap_lora_input_params_files = list(Path(ZH_RAP_LORA_ROOT_DIR).glob("*.json"))
-        self.zh_rap_lora_input_params_files += list(Path(ZH_RAP_LORA_ROOT_DIR).glob("*.json"))
+    def __init__(self, root_dir=DEFAULT_ROOT_DIR, lora_dir=None):
+        if lora_dir and lora_dir != "none":
+            self.lora_input_params_files = list(Path(lora_dir).glob("*.json"))
+        else:
+            self.root_dir = root_dir
+            self.input_params_files = list(Path(self.root_dir).glob("*.json"))
 
     def load_json(self, file_path):
         with open(file_path, "r", encoding="utf-8") as f:
             return json.load(f)
 
-    def sample(self, lora_name_or_path=None):
-        if lora_name_or_path is None or lora_name_or_path == "none":
-            json_path = random.choice(self.input_params_files)
-            json_data = self.load_json(json_path)
-        else:
-            json_path = random.choice(self.zh_rap_lora_input_params_files)
+    def sample(self):
+        if self.lora_input_params_files:
+            json_path = random.choice(self.lora_input_params_files)
             json_data = self.load_json(json_path)
             # Update the lora_name in the json_data
-            json_data["lora_name_or_path"] = lora_name_or_path
-
+            json_data["lora_name_or_path"] = self.lora_input_params_files
+        else:
+            json_path = random.choice(self.input_params_files)
+            json_data = self.load_json(json_path)
+        
         return json_data
