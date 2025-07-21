7e2426138ad54987df29766c243629710ad068a9
jarammm <jaramrammm@gmail.com>
2025-07-22 02:12:30 +0900

    u: refactor sampler

--- acestep/data_sampler.py
+++ acestep/data_sampler.py
@@ -1,30 +1,45 @@
 import json
 from pathlib import Path
-import random
 
-
-DEFAULT_ROOT_DIR = "examples/default/input_params"
 
 class DataSampler:
-    def __init__(self, root_dir=DEFAULT_ROOT_DIR, lora_dir=None):
-        if lora_dir and lora_dir != "none":
-            self.lora_input_params_files = list(Path(lora_dir).glob("*.json"))
-        else:
-            self.root_dir = root_dir
-            self.input_params_files = list(Path(self.root_dir).glob("*.json"))
+    def __init__(self, lora_dir="examples/pansori/input_params"):
+        self.lora_input_params_files = list(Path(lora_dir).glob("*.json"))
 
     def load_json(self, file_path):
         with open(file_path, "r", encoding="utf-8") as f:
             return json.load(f)
-
+    
+    def sample_data(self, json_data):
+        return (
+            json_data["audio_duration"],
+            json_data["prompt"],
+            json_data["lyrics"],
+            json_data["infer_step"],
+            json_data["guidance_scale"],
+            json_data["scheduler_type"],
+            json_data["cfg_type"],
+            json_data["omega_scale"],
+            ", ".join(map(str, json_data["actual_seeds"])),
+            json_data["guidance_interval"],
+            json_data["guidance_interval_decay"],
+            json_data["min_guidance_scale"],
+            json_data["use_erg_tag"],
+            json_data["use_erg_lyric"],
+            json_data["use_erg_diffusion"],
+            ", ".join(map(str, json_data["oss_steps"])),
+            json_data["guidance_scale_text"] if "guidance_scale_text" in json_data else 0.0,
+            (
+                json_data["guidance_scale_lyric"]
+                if "guidance_scale_lyric" in json_data
+                else 0.0
+            ),
+            json_data["audio_path"],
+            json_data["lora_name_or_path"],
+            json_data["lora_weight"]
+        )
+    
     def sample(self):
-        if self.lora_input_params_files:
-            json_path = random.choice(self.lora_input_params_files)
-            json_data = self.load_json(json_path)
-            # Update the lora_name in the json_data
-            json_data["lora_name_or_path"] = self.lora_input_params_files
-        else:
-            json_path = random.choice(self.input_params_files)
-            json_data = self.load_json(json_path)
-        
-        return json_data
+        json_data_list = [self.load_json(json_path) for json_path in self.lora_input_params_files]
+        sampled_json_data_list = [self.sample_data(json_data) for json_data in json_data_list]
+        return sampled_json_data_list
