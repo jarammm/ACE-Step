9f8da0f74d81d2d938edbc3263edcec47854965a
jarammm <jaramrammm@gmail.com>
2025-07-22 02:23:42 +0900

    c: add pansori config

--- acestep/vocab_setting.py
+++
@@ -1,4 +0,0 @@
-VOCAB_NAME = 'new_vocab'
-SPECIAL_TOKENS = ['[자진모리]', '[엇모리]', '[휘모리]', '[무장단]', '[아니리]', '[중중모리]', '[중모리]', '[진양조]', '[휘몰이]', '[창조]', '[자진중중모리]', '[자진몰이]', '[엇중모리]']
-TARGET_EMBED_NAME = 'lyric_embs'
-TARGET_INIT_VOCAB = '[verse]'
