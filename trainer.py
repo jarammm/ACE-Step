3b5da62de4bf15af470db8f7a8bffad37293218d
jarammm <jaramrammm@gmail.com>
2025-07-23 01:23:32 +0900

    u: remove ckpt save

--- trainer.py
+++ trainer.py
@@ -1,4 +1,4 @@
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.loggers import WandbLogger
 import wandb
@@ -634,17 +634,6 @@ class Pipeline(LightningModule):
 
         return total_loss
 
-    def on_save_checkpoint(self, checkpoint):
-        state = {}
-        log_dir = self.logger.save_dir
-        epoch = self.current_epoch
-        step = self.global_step
-        checkpoint_name = f"epoch={epoch}-step={step}_lora"
-        checkpoint_dir = os.path.join(log_dir, "checkpoints", checkpoint_name)
-        os.makedirs(checkpoint_dir, exist_ok=True)
-        self.transformers.save_lora_adapter(checkpoint_dir, adapter_name=self.adapter_name)
-        return state
-
     @torch.no_grad()
     def diffusion_process(
         self,
@@ -858,6 +847,18 @@ class Pipeline(LightningModule):
                 f.write(key_prompt_lyric)
             i += 1
 
+class SaveLoraCallback(Callback):
+    def __init__(self, adapter_name="default", every_n_steps=1000):
+        self.adapter_name = adapter_name
+        self.every_n_steps = every_n_steps
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        step = trainer.global_step
+        if step > 0 and step % self.every_n_steps == 0 and trainer.is_global_zero:
+            log_dir = trainer.logger.save_dir
+            checkpoint_dir = os.path.join(log_dir, "checkpoints", f"step={step}_lora")
+            os.makedirs(checkpoint_dir, exist_ok=True)
+            pl_module.transformers.save_lora_adapter(checkpoint_dir, adapter_name=self.adapter_name)
 
 def main(args):
     model = Pipeline(
@@ -872,14 +873,10 @@ def main(args):
         lora_config_path=args.lora_config_path,
         vocab_name=args.vocab_name
     )
-    checkpoint_callback = ModelCheckpoint(
-        monitor="val/loss",
-        mode="min",
-        save_top_k=0,
-        save_last=False,
-        every_n_train_steps=None,
-        save_on_train_epoch_end=False,
-        filename="{step}-{val/loss:.4f}",
+
+    lora_callback = SaveLoraCallback(
+        adapter_name=args.exp_name,
+        every_n_steps=args.every_n_train_steps
     )
     # add datetime str to version
     logger_callback = TensorBoardLogger(
@@ -906,7 +903,7 @@ def main(args):
         max_steps=args.max_steps,
         log_every_n_steps=1,
         logger=wandb_logger,
-        callbacks=[checkpoint_callback],
+        callbacks=[lora_callback],
         gradient_clip_val=args.gradient_clip_val,
         gradient_clip_algorithm=args.gradient_clip_algorithm,
         reload_dataloaders_every_n_epochs=args.reload_dataloaders_every_n_epochs,
@@ -927,7 +924,7 @@ if __name__ == "__main__":
     args.add_argument("--num_workers", type=int, default=8)
     args.add_argument("--epochs", type=int, default=-1)
     args.add_argument("--max_steps", type=int, default=4000)
-    args.add_argument("--every_n_train_steps", type=int, default=500)
+    args.add_argument("--every_n_train_steps", type=int, default=1000)
     args.add_argument("--dataset_path", type=str, default="./lora_dataset")
     args.add_argument("--exp_name", type=str, default="speaker_emb")
     args.add_argument("--precision", type=str, default="32")
@@ -940,7 +937,7 @@ if __name__ == "__main__":
     args.add_argument("--gradient_clip_algorithm", type=str, default="norm")
     args.add_argument("--reload_dataloaders_every_n_epochs", type=int, default=1)
     args.add_argument("--every_plot_step", type=int, default=1000)
-    args.add_argument("--val_check_interval", type=int, default=500)
+    args.add_argument("--val_check_interval", type=int, default=1000)
     args.add_argument("--lora_config_path", type=str, default="config/lora_config_v1.json")
     args.add_argument('--wandb_project', type=str, default="pansori-gen")
     args.add_argument('--wandb_name', type=str, default="speaker_emb")
